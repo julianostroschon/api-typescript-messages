2bf65ea3e634eb9a315c05a29194658791814c28
julianostroschon <julianostroschon@gmail.com>
2025-08-25 18:56:53 -0300

    🍱 Configuração dos testes

---
+++ test/jest.setup.ts
@@ -0,0 +1,13 @@
+import { config } from 'dotenv';
+
+// Carrega as variáveis de ambiente de teste
+config({ path: '.env.test' });
+
+// Configuração global para timeouts
+jest.setTimeout(30000);
+
+// Silencia logs durante os testes
+// jest.spyOn(console, 'log').mockImplementation(() => { });
+// jest.spyOn(console, 'info').mockImplementation(() => { });
+// jest.spyOn(console, 'warn').mockImplementation(() => { });
+// jest.spyOn(console, 'error').mockImplementation(() => { });
