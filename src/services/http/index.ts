8cd29ef9005fc8f1c4d67d7a55ba7cfab9d06492
julianostroschon <julianostroschon@gmail.com>
2025-08-30 20:01:31 -0300

    🔊 Add logger diferente ao routes

--- src/services/http/index.ts
+++ src/services/http/index.ts
@@ -2,29 +2,31 @@ import { cfg, parentLogger } from "@/infra";
 import fastify, { FastifyInstance } from "fastify";
 import { constructRoutes } from "./routes";
 
-export async function buildFastify(): Promise<FastifyInstance> {
+export async function buildFastify(isAlone: boolean): Promise<FastifyInstance> {
   const app = fastify();
-  const logger = parentLogger.child({ service: 'http' });
 
+  const logger = parentLogger.child({ service: 'routes' });
   app.get('/.ping', async (): Promise<{ hello: string }> => {
+    logger.info('Ping received');
     return { hello: 'world' };
   });
 
-  await constructRoutes(app, logger);
+  await constructRoutes(app, logger, isAlone);
 
   return app;
 }
 
 // entrypoint separado
-export async function startServer(): Promise<FastifyInstance> {
-  const app = await buildFastify();
+export async function startServer(isAlone: boolean): Promise<FastifyInstance> {
+  const logger = parentLogger.child({ service: 'http' });
+  const app = await buildFastify(isAlone);
 
   app.listen({ port: cfg.PORT }, (err: Error | null): void => {
     if (err) {
-      parentLogger.child({ service: 'http' }).error(err);
+      logger.error(err);
       process.exit(1);
     }
-    parentLogger.child({ service: 'http' }).info(`🌐 Server listening on port:${cfg.PORT}`);
+    logger.info(`🌐 Server listening on port:${cfg.PORT}`);
   });
 
   return app
